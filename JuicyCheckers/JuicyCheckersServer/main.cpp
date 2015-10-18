e98e24ef147adc950f05bc5572761c0f8ed79e1c
Jony <xyc8034@aut.ac.nz>
2015-10-18 14:24:44 +1300

    Adding the .gitignore to ignore the debug directory.

---
+++ JuicyCheckers/JuicyCheckersServer/main.cpp
@@ -0,0 +1,7 @@
+/*
+ *   Author:  Jony Hill <xyc8034@aut.ac.nz>
+ *   Filename: main.cpp
+ *   Description:  This is the dedicated server host for Juicy Checkers,  this server manages the hosting of
+ *					tourdanments 
+ *
+ */
