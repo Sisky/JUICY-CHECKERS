bb292dd7724f1edef30fe5d9e6c0f2f2554594ea
BrownClaw <davxmail@gmail.com>
2015-10-22 16:27:38 +1300

    current working code, selection working

---
+++ JuicyCheckers/JuicyCheckers/LineDrawing.h
@@ -0,0 +1,15 @@
+#ifndef __LINEDRAWING_H__
+#define __LINEDRAWING_H__
+
+// referecing : http://www.ogre3d.org/tikiwiki/tiki-index.php?page=DynamicLineDrawing
+
+
+
+class LineDrawing 
+{
+public:
+	LineDrawing(void);
+	~LineDrawing(void);
+};
+
+#endif __LINEDRAWING_H__
